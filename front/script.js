283cfe896a68c3821bd5de9b00fc2a80ce720f52
amirmohammad.b.p <amirmohammadbarari415@gmail.com>
2024-07-12 14:02:41 +0330

    switching bettwen in-up forms

--- front/script.js
+++ front/script.js
@@ -1,25 +1,45 @@
 const container = document.querySelector(".container");
 
-const signinBtn = document.querySelector(".show-signin-button");
 const signupBtn = document.querySelector(".show-signup-button");
-let is_showed = false;
+const signinBtn = document.querySelector(".show-signin-button");
+let page_state = "";
+let is_signin_form_close = true;
+let is_signup_form_close = true;
 
-signupBtn.addEventListener("click", doit);
-signinBtn.addEventListener("click", doitt);
-function doit() {
-    if (!is_showed) {
+signupBtn.addEventListener("click", signup_click);
+signinBtn.addEventListener("click", signin_click);
+function signin_click() {
+    if (is_signup_form_close) {
+        is_signin_form_close = !is_signin_form_close;
         container.classList.toggle("change-signin-form");
-        is_showed = true;
+        Log(is_signin_form_close, is_signup_form_close);
     }
-    else
-        container.classList.toggle("switch-bettwen-in-up");
+    else {
+        is_signup_form_close = true;
+        is_signin_form_close = false;
+        container.classList.remove("switch-to-signup");
+        container.classList.toggle("switch-to-signin");
+        Log(is_signin_form_close, is_signup_form_close);
+    }
 }
 
-function doitt() {
-    if (!is_showed) {
-        is_showed = true;
+function signup_click() {
+    if (is_signin_form_close) {
+        is_signup_form_close = !is_signup_form_close;
         container.classList.toggle("change-signup-form");
+        Log(is_signin_form_close, is_signup_form_close);
     }
-    else
-        container.classList.toggle("switch-bettwen-in-up");
+    else {
+        is_signin_form_close = true;
+        is_signup_form_close = false;
+
+        container.classList.remove("switch-to-signin");
+        container.classList.toggle("switch-to-signup");
+        Log(is_signin_form_close, is_signup_form_close);
+    }
+}
+
+function Log(is, iss) {
+    is ? console.log("ture.") : console.log("false.");
+    iss ? console.log("ture.") : console.log("false.");
 }
