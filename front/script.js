d80c5d85b7f68d43e42a7bd23f1cc301656b757a
amirmohammad.b.p <amirmohammadbarari415@gmail.com>
2024-07-12 19:25:36 +0330

    add title and fix some bug
    switch title isn't working good yet

--- front/script.js
+++ front/script.js
@@ -2,6 +2,8 @@ const container = document.querySelector(".container");
 
 const signupBtn = document.querySelector(".show-signup-button");
 const signinBtn = document.querySelector(".show-signin-button");
+const title1 = document.querySelector("elegantshadow");
+const title2 = document.querySelector("elegantshadow1");
 
 let is_signin_form_close = true;
 let is_signup_form_close = true;
@@ -11,29 +13,42 @@ signinBtn.addEventListener("click", signin_click);
 function signin_click() {
     if (is_signup_form_close) {
         is_signin_form_close = !is_signin_form_close;
+        title_state();
         container.classList.toggle("change-signin-form");
-        Log(is_signin_form_close, is_signup_form_close);
+        container.classList.remove("switch-to-signup");
     }
     else {
         is_signup_form_close = true;
         is_signin_form_close = false;
+        title_state();
         container.classList.remove("switch-to-signup");
         container.classList.toggle("switch-to-signin");
-        Log(is_signin_form_close, is_signup_form_close);
     }
 }
 
 function signup_click() {
     if (is_signin_form_close) {
         is_signup_form_close = !is_signup_form_close;
+        title_state();
         container.classList.toggle("change-signup-form");
-        Log(is_signin_form_close, is_signup_form_close);
+        container.classList.remove("switch-to-signup");
     }
     else {
         is_signin_form_close = true;
         is_signup_form_close = false;
+        title_state();
         container.classList.remove("switch-to-signin");
         container.classList.toggle("switch-to-signup");
-        Log(is_signin_form_close, is_signup_form_close);
+    }
+}
+
+function title_state() {
+    if (!is_signin_form_close && is_signup_form_close) {
+        container.classList.toggle("hide-title");
+        container.classList.toggle("hide-title1");
+    }
+    else {
+        container.classList.remove("hide-title");
+        container.classList.remove("hide-title1");
     }
 }
