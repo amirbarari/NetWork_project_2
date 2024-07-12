931f786fbfeb6f9fd7800107b648456400cb76a9
amirmohammad.b.p <amirmohammadbarari415@gmail.com>
2024-07-12 11:30:48 +0330

    switchig bettwen signup and signin div

--- front/script.js
+++ front/script.js
@@ -1,7 +1,25 @@
 const container = document.querySelector(".container");
-const signupBtn = document.querySelector(".show-signup-button");
-signupBtn.addEventListener("click", doit);
 
+const signinBtn = document.querySelector(".show-signin-button");
+const signupBtn = document.querySelector(".show-signup-button");
+let is_showed = false;
+
+signupBtn.addEventListener("click", doit);
+signinBtn.addEventListener("click", doitt);
 function doit() {
-    container.classList.toggle("change");
+    if (!is_showed) {
+        container.classList.toggle("change-signin-form");
+        is_showed = true;
+    }
+    else
+        container.classList.toggle("switch-bettwen-in-up");
+}
+
+function doitt() {
+    if (!is_showed) {
+        is_showed = true;
+        container.classList.toggle("change-signup-form");
+    }
+    else
+        container.classList.toggle("switch-bettwen-in-up");
 }
