708e3d60f8f58a1b5e9246f7e89c393a68e3582f
amirmohammad.b.p <amirmohammadbarari415@gmail.com>
2024-07-14 23:16:58 +0330

    handle POST methods in log-rig page

--- profileApp/routes.py
+++ profileApp/routes.py
@@ -1,10 +1,15 @@
 from profileApp import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route("/")
-
-@app.route("/logrig/")
+@app.route("/logrig/", methods=['GET', 'POST'])
 def log_rig():
+    form_name = request.form.get("name")
+    if form_name == "sign-in-form":
+        user_name = request.form["user-name"]
+        password = request.form["password"]
+    elif form_name == "sign-up-form":
+        name = request.form["name"]
     return render_template("LogRig.html")
 
 @app.route("/profile/<user_name>")
