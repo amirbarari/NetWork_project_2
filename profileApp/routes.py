5f6fc5b5c22eb551254951eb2b1f7a74a0d5b371
amirmohammad.b.p <amirmohammadbarari415@gmail.com>
2024-07-14 21:18:07 +0330

    create profile page and added to routes

--- profileApp/routes.py
+++ profileApp/routes.py
@@ -6,3 +6,7 @@ from flask import render_template
 @app.route("/logrig/")
 def log_rig():
     return render_template("LogRig.html")
+
+@app.route("/profile/<user_name>")
+def user_profile(user_name):
+    return f"user with username : {user_name} loged in"
